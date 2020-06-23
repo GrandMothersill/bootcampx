2213e0082e9075c30b58308fb8e860bb88e33ef5
Graham Mothersill <graham.mothersill@gmail.com>
2020-06-23 18:26:51 +0000

    create 4-queries with 13 queries inside

--- 4-queries/1_teacher_total_assistance_requests.sql
+++ 4-queries/1_teacher_total_assistance_requests.sql
@@ -1,2 +1,5 @@
-SELECT COUNT(assistance_requests.*)
-FROM assistance_requests;
+SELECT count(assistance_requests.*) as total_assistances, teachers.name
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+WHERE name = 'Waylon Boehm'
+GROUP BY teachers.name;
